d92ae4f6b3d11d798968d7ef55d2551603db7f21
KoaChu <kozachu11@gmail.com>
2020-03-20 00:35:11 -0700

    styled modal a little

--- src/components/upload-modal/upload-modal.jsx
+++ src/components/upload-modal/upload-modal.jsx
@@ -9,21 +9,18 @@ import './upload-modal.scss';
 
 const UploadModal = () => {
     return (
-        <Popup trigger={<button className="open-button" id='modal-button'></button>} modal>
+        <Popup className='pop-up' trigger={<button className="open-button" id='modal-button'></button>} modal>
 		    {close => (
 		      <div className="modal">
 		        <a className="close" onClick={close}>
 		          &times;
 		        </a>
+		        <div className='modal-header'>
+		        	<h4>Post header</h4>
+		        </div>
 		        <div className="content">
 		          {" "}
-		          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
-		          Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
-		          delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
-		          <br />
-		          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
-		          commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae
-		          explicabo nemo nam libero ad, doloribus, voluptas rem alias. Vitae?
+		          Upload button
 		        </div>
 		        <div className="actions">
 		          <Popup
