60d0629f98b53a9f7b6fffca48f5bd7b2322a311
KoaChu <kozachu11@gmail.com>
2020-04-08 00:29:11 -0700

    styling and adding profile image to header

--- src/components/profile-icon/profile-icon.jsx
+++ src/components/profile-icon/profile-icon.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { toggleProfileHidden } from '../../redux/profile/profile-actions';
+import { selectUserImage } from '../../redux/user/user-selectors';
 
 import { ReactComponent as DefaultIcon } from '../../assets/default-user.svg';
 
 import './profile-icon.scss';
 
 const ProfileIcon = ({ toggleProfileHidden }) => {
+	const [userImg, setUserImg] = useState('');
+
+	useEffect(() => {
+		setUserImg(JSON.parse(localStorage.getItem('currentUser')).imageUrl)
+	},[]);
+	
     return (
         <div className='profile-icon' onClick={toggleProfileHidden}>
-        	<DefaultIcon className='icon' />
+        	{ userImg ? <div className='icon-wrapper'><img src={userImg} alt='profile' className='user-icon' /></div> : <DefaultIcon className='icon' /> }
         </div>
     );
 };
