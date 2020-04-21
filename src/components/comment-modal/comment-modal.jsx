ffa8814f02acd5a251cbfd140d49ec6415e9a792
KoaChu <kozachu11@gmail.com>
2020-04-21 02:45:51 -0700

    adding functionality to note modal

--- src/components/comment-modal/comment-modal.jsx
+++ src/components/comment-modal/comment-modal.jsx
@@ -1,21 +1,31 @@
 import React, { useState } from 'react';
 import Popup from 'reactjs-popup';
 
+import CustomButton from '../custom-button/custom-button';
+
 import { ReactComponent as NotesIcon } from '../../assets/black-notes.svg'
 
 import './comment-modal.scss';
 
-const CommentModal = ({ className }) => {
+const CommentModal = ({ imgSrc }) => {
 
     return (
-        <div className='comment-modal'>
+    	<div className='notes-modal'>
+	        <div className='comment-modal'>
+	        </div>
         	<a class="comment-close" href='#'>&times;</a>
         	<div className='comment-image'>
-        		<img src='' alt='post-image' />
+        		<img src={imgSrc} alt='post-image' />
         	</div>
         	<div className='comment-area'>
-        		<span className='comment-header'>Notes</span>
-        		<div className='comments'></div>
+        		<span className='comment-header'>@Username</span>
+        		<div className='comments'>
+
+        		</div>
+        		<div className='note-input'>
+    				<textarea className='note-text' id='note-text' rows={1} type='text' placeholder='Post a note...' onChange={() => {console.log('hi');}} required />
+        		</div>
+        		<CustomButton id='note-button'>Post</CustomButton>
         	</div>
         </div>
     );
--- src/pages/searchpage/searchpage.jsx
+++ src/pages/searchpage/searchpage.jsx
@@ -4,15 +4,11 @@ import './searchpage.scss';
 
 import LoadingIndicator from '../../components/loading-indicator/loading-indicator';
 import ProgressBar from '../../components/progress-bar/progress-bar';
-import CommentModal from '../../components/comment-modal/comment-modal';
-
-import { ReactComponent as Notes } from '../../assets/notes.svg';
 
 const SearchPage = () => {
     return (
         <div className='searchpage'>
         	<h1>Search Page</h1>
-        	<CommentModal />
         	<LoadingIndicator />
         	<ProgressBar />
         </div>
